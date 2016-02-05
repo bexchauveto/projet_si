525f3268a2df9d2a858738b206b6cf865cfdbf67
Bex-Chauvet <olivier.bexchauvet@gmail.com>
2016-02-05 08:57:23 +0100

    Premier commit

---
+++ lex.yy.c
@@ -0,0 +1,24 @@
+%{
+
+
+
+%}
+
+
+
+%%
+/*
+(,),{,},main, const, int, id ([a-zA-Z_][a-zA-Z0-9_]*), +, -, *, /, =, espace, tab, virgule, \n, ;, print,
+decimal et exponentielle
+*/
+
+
+
+%%
+
+
+void main()
+{
+	yylex();
+
+}
