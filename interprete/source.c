8c33f33de1a9fe8fceb8d00b3dfbb45d00eafac9
a-laine <laineaurelien@gmail.com>
2016-02-21 17:14:20 +0100

    modif interprete

--- interprete/source.c
+++ interprete/source.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 
 
-#define SIZE_TABS 1000
-
 typedef struct {
 	char code;
 	int op[3];
@@ -15,14 +13,12 @@ typedef struct {
 } InstInfo;
 
 
-
-FILE* fichier;
-
-int R_IP;
+#define SIZE_TABS 1000
 Instruction code[SIZE_TABS];
 int data[SIZE_TABS];
+int R_IP;
 
-
+#define ERR_CODE '\0'
 const InstInfo tabInstInfo[] = {
 	{ '1', 3 },
 	{ '2', 3 },
@@ -36,16 +32,24 @@ const InstInfo tabInstInfo[] = {
 	{ 'A', 3 },
 	{ 'B', 3 },
 	{ 'C', 3 },
-	{ '\0', 0 },
+	{ ERR_CODE, 0 },
 };
 
 
-
-int test(int addr) { return addr>=0 && addr<SIZE_TABS; }
-int readInst(Instruction* ins, int nbOp);
-int readFile();
+int getNbOp(char code)
+{
+	for(j=0; tabInstInfo[j].code != ERR_CODE; j++)
+		if(tabInstInfo[j].code == code)
+			return tabInstInfo[j].nbOp;
+	return -1;
+}
+// lecture
+int readInst(char* line, Instruction* ins, int nbOp);
+int readFile(FILE* fichier);
+// execute
 int execute();
 int execInst(Instruction* ins);
+// divers
 void afficheData(int indice, int nb);
 void afficheInst(int indice, int nb);
 
@@ -74,21 +78,7 @@ int main(int argc, char *argv[])
     }
     
     printf("Lecture du fichier...\n");
-    error = readFile();
-    switch(error)
-    {
-    	case -1:
-    		printf("Erreur : code operation\n");
-    		break;
-    	case -2:
-    		printf("Erreur : operande manquante\n");
-    		break;
-    	case -3:
-    		printf("Erreur : mauvaise operande\n");
-    		break;
-    	default:
-    		break;
-    }
+    error = readFile(fichier);
     
     fclose(fichier);
     
@@ -103,52 +93,53 @@ int main(int argc, char *argv[])
 
 /******         Lecture         ******/
 
-int readFile()
+int readFile(FILE* fichier)
 {
 	int i, j;
 	int error = 0;
 	char c = 0;
-	for(i=1; 1; i++)
+	char line[100];
+	Instruction ins;
+	
+	for(i=1; (error = fgets(line,sizeof(line)-1,fichier)); i++)
 	{
-		Instruction ins;
-		
-		error = fscanf(fichier, "%c",&c);
-		if(error != 1)
-			return -1;
-		ins.code = c;
-		
-		for(j=0; tabInstInfo[j].code != '\0'; j++)
-		{
-			if(tabInstInfo[j].code == c)
-			{
-				error = readInst(&ins, tabInstInfo[j].nbOp);
-				break;
-			}
+		// lecture code operation
+		error = sscanf(fichier, "%c",&(ins.code))
+		if(error != 1) {
+			printf("Erreur : lecture code operation\n");
+			continue;
 		}
-		if(tabInstInfo[j].code == '\0')
-			error = -1;
-		if(error != 0)
-			break;
+		
+		// lectures operandes
+		error = getNbOp(ins.code);
+		if(error == -1) {
+			printf("Erreur : nombre d'operandes inconnu pour le code (%c)\n", ins.code);
+			continue;
+		}
+		error = readInst(line, &ins, error);
+		if(error == -2) {
+			printf("Erreur : nombre d'operandes invalide pour le code (%c)\n", ins.code);
+			continue;
+		}
 		
 		code[i] = ins;
 	}
 	
 	i++;
-	code[i].code = '\0';
+	code[i].code = ERR_CODE;
+	code[0].code = ERR_CODE;
 	
-	return error;
+	return 0;
 }
 
-int readInst(Instruction* ins, int nbOp)
+int readInst(char* line, Instruction* ins, int nbOp)
 {
 	int error, i;
 	for(i=0; i<nbOp; i++)
 	{
-		error = fscanf(fichier, "%d", &(ins->op[i]));
+		error = sscanf(line, "%d", &(ins->op[i]));
 		if(error != 1)
 			return -2;
-		if(!test(ins->op[i]))
-			return -3;
 	}
 	return 0;
 }
@@ -168,6 +159,10 @@ int execute()
 		ins = code[R_IP];
 		error = execInst(&ins);
 	}
+	if(error == -1)
+		printf("Erreur : addresse invalide (data)\n");
+	if(error == -2)
+		printf("Erreur : addresse invalide (code)\n");
 	return error;
 }
 
@@ -176,6 +171,9 @@ int val(Instruction* ins, int op)
 
 int* p_val(Instruction* ins, int op)
 { return &data[ins->op[op]]; }
+
+int testData(Instruction* ins, int op)
+{ return (ins->op[op])>=0 && (ins->op[op])<sizeof(data); }
 
 int execInst(Instruction* ins)
 {
@@ -189,45 +187,69 @@ int execInst(Instruction* ins)
 		case 0:
 			return 1;
 		case '1':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) + val(ins,2);
 			break;
 		case '2':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) * val(ins,2);
 			break;
 		case '3':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) - val(ins,2);
 			break;
 		case '4':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) / val(ins,2);
 			break;
 		case '5':
+			if(!testData(ins,0) || !testData(ins,1))
+				return -1;
 			*p_val(ins,0) = val(ins,1);
 			break;
 		case '6':
+			if(!testData(ins,0))
+				return -1;
 			*p_val(ins,0) = ins->op[1];
 			break;
 		case '7':
 			R_IP = ins->op[0];
 			break;
 		case '8':
+			if(!testData(ins,0))
+				return -1;
 			if(val(ins,0) == 0)
 				R_IP = ins->op[1];
 			break;
 		case '9':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) < val(ins,2);
 			break;
 		case 'A':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) > val(ins,2);
 			break;
 		case 'B':
+			if(!testData(ins,0) || !testData(ins,1) || !testData(ins,2))
+				return -1;
 			*p_val(ins,0) = val(ins,1) == val(ins,2);
 			break;
 		case 'C':
+			if(!testData(ins,0))
+				return -1;
 			printf("%d\n",val(ins,0));
 			break;
 		default:
 			break;
 	}
+	if(R_IP<0 && R_IP>=sizeof(code))
+		return -2;
 	return 0;
 }
 
